4952cd38ed2b4e53450c8f8a5693dff58f47327c
pastorvaldez47-arch <pastorvaldez47@gmail.com>
2025-12-05 16:09:19 -0400

    Add files via upload

---
+++ server.js.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " +
